9c9252e196e6c82ac6cd6ef95c5d50b8fd8a3f8c
Jayaraj Sivadasan <jayaraj.sivadasan@gmail.com>
2019-11-10 19:04:58 -0500

    Included connectionstring, injected DbContext in Startup and initalized DbContext constructor

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,28 @@
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config; 
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+         services.AddDbContext<DutchContext>(c=>c.UseSqlServer(_config.GetConnectionString("Default")));
             services.AddTransient<IEmailService, EmailService>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
